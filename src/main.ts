5de27f41ba585212423028581699ecf53570b6de
Mchim91 <chimenem.mbato99@gmail.com>
2024-11-26 14:41:50 +0100

    farm-api-setup

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,32 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJs App - farm budget API')
+    .setDescription('Use the base API URL as http://localhost:3000/api')
+    .setTermsOfService('http://localhost:3000/api')
+    .setLicense(
+      'MIT License',
+      'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt'
+    )
+    .addServer('http://localhost:3000/api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('v1/api', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
